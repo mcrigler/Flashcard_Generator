73437f4b983c4d1fe1f029f6da413de00a7522bf
MDC_Desktop <marya.crigler@att.net>
2017-06-25 08:06:26 -0500

    Initial commit  20170625

---
+++ flashcards.js
@@ -0,0 +1,32 @@
+/************************************************//
+//*****       set required npm modules       *****//
+//************************************************//
+//
+//-- file system  --//
+	var fs = require("fs");
+
+//-- inquirer  --//
+	var inquirer = require("inquirer");
+
+//-- timestamp
+	var timestamp = require("time-stamp");
+
+//-- flashcards
+	var basicCard = require("./basic.js")
+	var clozeCard = require("./cloze.js")
+
+
+
+
+//************************************************//
+//*****    declare global variables          *****//
+//************************************************//
+//
+	
+	
+
+
+//************************************************//
+//*****             master inputs            *****//
+//************************************************//
+//
